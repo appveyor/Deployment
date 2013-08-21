3e71019e2241c8701b16a074ecfe64060fa7be9a
Feodor Fitsner <feodor@fitsner.com>
2013-08-21 12:18:42 -0700

    Download deployment scripts during install

--- install.ps1
+++ install.ps1
@@ -8,3 +8,8 @@ New-Item "$modules\AppRolla" -ItemType Directory -Force | Out-Null
 # install AppVeyor module
 New-Item "$modules\AppVeyor" -ItemType Directory -Force | Out-Null
 (New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/AppVeyor-PowerShell/master/AppVeyor.psm1", "$modules\AppVeyor\AppVeyor.psm1")
+
+# download deployment script to the current directory
+$path = Resolve-Path .\
+(New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/Deployment/master/config.ps1", "$path\config.ps1")
+(New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/Deployment/master/deploy.ps1", "$path\deploy.ps1")
