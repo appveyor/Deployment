e83ee4ca6766d3918f7c086979ac84316b735c28
Feodor Fitsner <feodor@fitsner.com>
2013-08-21 12:22:49 -0700

    Add messages to install script

--- install.ps1
+++ install.ps1
@@ -1,16 +1,15 @@
-﻿# install modules to your user profile
+﻿Write-Host "Installing AppRolla and AppVeyor modules into your user profile"
 $modules = "$home\Documents\WindowsPowerShell\Modules"
 
-# install AppRolla module
+Write-Host "Installing AppRolla module"
 New-Item "$modules\AppRolla" -ItemType Directory -Force | Out-Null
 (New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/AppRolla/master/AppRolla.psm1", "$modules\AppRolla\AppRolla.psm1")
 
-# install AppVeyor module
+Write-Host "Installing AppVeyor module"
 New-Item "$modules\AppVeyor" -ItemType Directory -Force | Out-Null
 (New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/AppVeyor-PowerShell/master/AppVeyor.psm1", "$modules\AppVeyor\AppVeyor.psm1")
 
-# download deployment script to the current directory
+Write-Host "Downloading deployment scripts to the current directory"
 $path = Resolve-Path .\
-$path
 (New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/Deployment/master/config.ps1", "$path\config.ps1")
 (New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/Deployment/master/deploy.ps1", "$path\deploy.ps1")
